09ef45d9c627d1bbf34b50c7ed64b6a732d2f919
Ralf Schmitt <ralf@brainbot.com>
2023-10-31 14:32:11 +0100

    Add a deploy script
    The deployKeyperSet currently contains dummy addresses and needs to be changed
    for the real setup.

--- README.md
+++ README.md
@@ -5,3 +5,20 @@
 Install [foundry](https://book.getfoundry.sh/getting-started/installation).
 
 Run `forge test -vvv`
+
+## Using the deploy script
+
+Create a `.env` file with the following content, fill in the missing values:
+
+```shell
+GOERLI_RPC_URL=https://ethereum-goerli.publicnode.com
+PRIVATE_KEY=0xe1...
+ETHERSCAN_API_KEY=BIR44...
+```
+
+Run the following command:
+
+```shell
+source .env
+forge script script/deploy.s.sol --rpc-url $GOERLI_RPC_URL --broadcast --verify -vvvv
+```
---
+++ script/deploy.s.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import "../src/KeyBroadcastContract.sol";
+import "../src/KeyperSet.sol";
+import "../src/KeyperSetManager.sol";
+import "../src/Sequencer.sol";
+import "../src/ValidatorRegistry.sol";
+
+contract DeployAll is Script {
+    KeyBroadcastContract public keyBroadcastContract;
+    KeyperSet public keyperSet;
+    KeyperSetManager public keyperSetManager;
+    Sequencer public sequencer;
+    ValidatorRegistry public validatorRegistry;
+
+    function deployKeyperSet() public {
+        keyperSet = new KeyperSet();
+        address[] memory members = new address[](3);
+        members[0] = address(1);
+        members[1] = address(2);
+        members[2] = address(3);
+        keyperSet.addMembers(members);
+        keyperSet.setFinalized();
+    }
+
+    function deployKeyperSetManager() public {
+        keyperSetManager = new KeyperSetManager();
+        keyperSetManager.addKeyperSet(0, address(keyperSet));
+    }
+
+    function deployKeyBroadcastContract() public {
+        keyBroadcastContract = new KeyBroadcastContract(
+            address(keyperSetManager)
+        );
+    }
+
+    function deploySequencer() public {
+        sequencer = new Sequencer();
+    }
+
+    function deployValidatorRegistry() public {
+        validatorRegistry = new ValidatorRegistry();
+    }
+
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+        deployKeyperSet();
+        deployKeyperSetManager();
+        deployKeyBroadcastContract();
+        deploySequencer();
+        deployValidatorRegistry();
+        vm.stopBroadcast();
+    }
+}
